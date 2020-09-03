7adf28191c61cdfa80e36b8e4d8ebe61286f4b19
Daniel Paulus <daniel.paulus@mesmerhq.com>
2020-09-03 14:32:10 +0200

    clean up processcontrol

--- usbmux/instruments/processcontrol.go
+++ usbmux/instruments/processcontrol.go
@@ -8,55 +8,62 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const channelName = "com.apple.instruments.server.services.processcontrol"
+const serviceName string = "com.apple.instruments.remoteserver"
+const processControlChannelName = "com.apple.instruments.server.services.processcontrol"
 
-type ProcessControl struct {
+type processControl struct {
 	processControlChannel *dtx.Channel
 }
 
-type ProcessControlDispatcher struct{}
+type processControlDispatcher struct{}
 
+//KillApp kills an app with the given PID on the given device.
 func KillApp(pid uint64, device usbmux.DeviceEntry) error {
-	conn, _ := dtx.NewConnection(device, "com.apple.instruments.remoteserver")
+	conn, _ := dtx.NewConnection(device, serviceName)
 	//defer conn.Close()
-	processControl := NewProcessControl(conn)
+	processControl := newProcessControl(conn)
 	return processControl.KillProcess(pid)
 }
 
+//LaunchApp launches the app with the given bundleID on the given device.LaunchApp
+//Use LaunchAppWithArgs for passing arguments and envVars. It returns the PID of the created app process.
 func LaunchApp(bundleID string, device usbmux.DeviceEntry) (uint64, error) {
-	conn, _ := dtx.NewConnection(device, "com.apple.instruments.remoteserver")
-	//defer conn.Close()
-	processControl := NewProcessControl(conn)
+	conn, _ := dtx.NewConnection(device, serviceName)
+	//defer conn.Close()const
+	processControl := newProcessControl(conn)
 	options := map[string]interface{}{}
 	options["StartSuspendedKey"] = uint64(0)
 	return processControl.StartProcess(bundleID, map[string]interface{}{}, []interface{}{}, options)
 }
 
+//LaunchAppWithArgs same as LaunchApp but passes arguments, envVars and options.
 func LaunchAppWithArgs(bundleID string, device usbmux.DeviceEntry, args []interface{}, env map[string]interface{}, opts map[string]interface{}) (uint64, error) {
-	conn, _ := dtx.NewConnection(device, "com.apple.instruments.remoteserver")
+	conn, _ := dtx.NewConnection(device, serviceName)
 	//defer conn.Close()
-	return NewProcessControl(conn).StartProcess(bundleID, env, args, opts)
+	return newProcessControl(conn).StartProcess(bundleID, env, args, opts)
 }
 
-func (p ProcessControlDispatcher) Dispatch(m dtx.Message) {
+func (p processControlDispatcher) Dispatch(m dtx.Message) {
 	log.Debug(m)
 }
 
-func NewProcessControl(dtxConnection *dtx.Connection) ProcessControl {
-	processControlChannel := dtxConnection.RequestChannelIdentifier(channelName, ProcessControlDispatcher{})
-	return ProcessControl{processControlChannel: processControlChannel}
+func newProcessControl(dtxConnection *dtx.Connection) processControl {
+	processControlChannel := dtxConnection.RequestChannelIdentifier(processControlChannelName, processControlDispatcher{})
+	return processControl{processControlChannel: processControlChannel}
 }
 
-func (p ProcessControl) KillProcess(pid uint64) error {
+//KillProcess kills the process on the device.
+func (p processControl) KillProcess(pid uint64) error {
 	_, err := p.processControlChannel.MethodCall("killPid:", pid)
 	return err
 }
 
-func (p ProcessControl) StartProcess(bundleID string, envVars map[string]interface{}, arguments []interface{}, options map[string]interface{}) (uint64, error) {
+//StartProcess launches an app on the device using the bundleID and optional envvars, arguments and options. It returns the PID.
+func (p processControl) StartProcess(bundleID string, envVars map[string]interface{}, arguments []interface{}, options map[string]interface{}) (uint64, error) {
 	//seems like the path does not matter
 	const path = "/private/"
 
-	log.WithFields(log.Fields{"channel_id": channelName, "bundleID": bundleID}).Info("Launching process")
+	log.WithFields(log.Fields{"channel_id": processControlChannelName, "bundleID": bundleID}).Info("Launching process")
 
 	msg, err := p.processControlChannel.MethodCall(
 		"launchSuspendedProcessWithDevicePath:bundleIdentifier:environment:arguments:options:",
@@ -66,13 +73,13 @@ func (p ProcessControl) StartProcess(bundleID string, envVars map[string]interfa
 		arguments,
 		options)
 	if err != nil {
-		log.WithFields(log.Fields{"channel_id": channelName, "error": err}).Info("failed starting process")
+		log.WithFields(log.Fields{"channel_id": processControlChannelName, "error": err}).Info("failed starting process")
 	}
 	if msg.HasError() {
 		return 0, fmt.Errorf("Failed starting process: %s", msg.Payload[0])
 	}
 	if pid, ok := msg.Payload[0].(uint64); ok {
-		log.WithFields(log.Fields{"channel_id": channelName, "pid": pid}).Info("Process started successfully")
+		log.WithFields(log.Fields{"channel_id": processControlChannelName, "pid": pid}).Info("Process started successfully")
 		return pid, nil
 	}
 	return 0, fmt.Errorf("pid returned in payload was not of type uint64 for processcontroll.startprocess, instead: %s", msg.Payload)
