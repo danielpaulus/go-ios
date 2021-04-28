3262595009b05f0c1f6f5d96184ee68dec30b59d
nil <appl3_me@qq.com>
2021-04-28 17:56:34 +0800

    add pcap file export

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/google/uuid v1.1.2
+	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/sys v0.0.0-20200831180312-196b9ba8737a // indirect
--- ios/pcap/pcap.go
+++ ios/pcap/pcap.go
@@ -1,11 +1,43 @@
 package pcap
 
 import (
-	"encoding/hex"
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
 
 	"github.com/danielpaulus/go-ios/ios"
+	"github.com/lunixbochs/struc"
 	"howett.net/plist"
 )
+
+// struct pcap_hdr_s {
+//         guint32 magic_number;   /* magic number */
+//         guint16 version_major;  /* major version number */
+//         guint16 version_minor;  /* minor version number */
+//         gint32  thiszone;       /* GMT to local correction */
+//         guint32 sigfigs;        /* accuracy of timestamps */
+//         guint32 snaplen;        /* max length of captured packets, in octets */
+//         guint32 network;        /* data link type */
+// } pcap_hdr_t;
+
+// typedef struct pcaprec_hdr_s {
+//         guint32 ts_sec;         /* timestamp seconds */
+//         guint32 ts_usec;        /* timestamp microseconds */
+//         guint32 incl_len;       /* number of octets of packet saved in file */
+//         guint32 orig_len;       /* actual length of packet */
+// } pcaprec_hdr_t;
+type PcapPlistHeader struct {
+	HdrSize        int `struc:"uint32,big"`
+	Xxx            int `struc:"uint8,big"`
+	PacketSize     int `struc:"uint32,big"`
+	Flag1          int `struc:"uint32,big"`
+	Flag2          int `struc:"uint32,big"`
+	OffsetToIPData int `struc:"uint32,big"`
+	Zero           int `struc:"uint32,big"`
+}
 
 func Start(device ios.DeviceEntry) error {
 	intf, err := ios.ConnectToService(device, "com.apple.pcapd")
@@ -13,6 +45,11 @@ func Start(device ios.DeviceEntry) error {
 		return err
 	}
 	plistCodec := ios.NewPlistCodec()
+	f, err := createPcap(fmt.Sprintf("dump-%d.pcap",time.Now().Unix()))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 	for {
 		b, err := plistCodec.Decode(intf.Reader())
 		if err != nil {
@@ -22,7 +59,15 @@ func Start(device ios.DeviceEntry) error {
 		if err != nil {
 			return err
 		}
-		println(hex.Dump(decodedBytes))
+		packet, err := getPacket(decodedBytes)
+		if err != nil {
+			return err
+		}
+
+		err = writePacket(f, packet)
+		if err != nil {
+			return err
+		}
 	}
 }
 
@@ -30,4 +75,66 @@ func fromBytes(data []byte) ([]byte, error) {
 	var result []byte
 	_, err := plist.Unmarshal(data, &result)
 	return result, err
+}
+
+func createPcap(name string) (*os.File, error) {
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	if err != nil {
+		return nil, err
+	}
+	f.Write([]byte{
+		0xd4, 0xc3, 0xb2, 0xa1, 0x02, 0x00, 0x04, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0xff, 0xff, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00})
+	return f, nil
+}
+
+type PcaprecHdrS struct {
+	TsSec   int `struc:"uint32,little"` /* timestamp seconds */
+	TsUsec  int `struc:"uint32,little"` /* timestamp microseconds */
+	InclLen int `struc:"uint32,little"` /* number of octets of packet saved in file */
+	OrigLen int `struc:"uint32,little"` /* actual length of packet */
+}
+
+func writePacket(f *os.File, packet []byte) error {
+	now := time.Now()
+	phs := &PcaprecHdrS{
+		int(now.UTC().Unix()),
+		int(now.UTC().UnixNano() * 1000),
+		len(packet),
+		len(packet),
+	}
+	var buf bytes.Buffer
+	err := struc.Pack(&buf, phs)
+	if err != nil {
+		return err
+	}
+	f.Write(buf.Bytes())
+	f.Write(packet)
+	return nil
+}
+
+func getPacket(buf []byte) ([]byte, error) {
+	pph := PcapPlistHeader{}
+	preader := bytes.NewReader(buf)
+	struc.Unpack(preader, &pph)
+
+	interfacetype := make([]byte, pph.HdrSize-25)
+	cnt, err := preader.Read(interfacetype)
+	if err != nil {
+		return []byte{}, err
+	}
+	if cnt != pph.HdrSize-25 {
+		return []byte{}, errors.New("invalid HdrSize")
+	}
+	fmt.Println(interfacetype)
+	packet, err := ioutil.ReadAll(preader)
+	if err != nil {
+		return packet, err
+	}
+	if pph.OffsetToIPData == 0 {
+		ext := []byte{0xbe, 0xfe, 0xbe, 0xfe, 0xbe, 0xfe, 0xbe, 0xfe, 0xbe, 0xfe, 0xbe, 0xfe, 0x08, 0x00}
+		return append(ext, packet...), nil
+	}
+	return packet, nil
 }
