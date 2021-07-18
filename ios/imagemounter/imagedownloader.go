1153731d97a3d38a7c344a3d84598308c25da9b5
Daniel Paulus <daniel.paulus@icloud.com>
2021-07-18 13:06:23 +0200

    fixed last issues, images are only downloaded once, __MACOSX folders are cleaned

--- ios/imagemounter/imagedownloader.go
+++ ios/imagemounter/imagedownloader.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
 const repo = "https://github.com/haikieu/xcode-developer-disk-image-all-platforms/raw/master/DiskImages/iPhoneOS.platform/DeviceSupport/%s.zip"
@@ -25,9 +27,9 @@ func DownloadImageFor(device ios.DeviceEntry, baseDir string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	if imageDownloaded {
-		log.Infof("iOS %s developer image already downloaded from https://github.com/haikieu/", version)
-		return "build path here", nil
+	if imageDownloaded != "" {
+		log.Infof("%s already downloaded from https://github.com/haikieu/", imageDownloaded)
+		return imageDownloaded, nil
 	}
 
 	log.Infof("getting developer image for iOS %s", version)
@@ -48,31 +50,56 @@ func DownloadImageFor(device ios.DeviceEntry, baseDir string) (string, error) {
 		log.Warnf("failed deleting: '%s' with err: %+v", zipFileName, err)
 	}
 	log.Infof("downloaded: %+v totalbytes: %d", files, size)
-	return "", nil
+	downloadedDmgPath, err := findImage(path.Join(baseDir, imagepath), version)
+	if err != nil {
+		return "", err
+	}
+	os.RemoveAll(path.Join(baseDir, imagepath, "__MACOSX"))
+
+	log.Infof("Done extracting: %s", downloadedDmgPath)
+	return downloadedDmgPath, nil
 }
 
-func validateBaseDirAndLookForImage(baseDir string, version string) (bool, error) {
+func findImage(dir string, version string) (string, error) {
+	imageToFind := fmt.Sprintf("%s/%s", version, developerDiskImageDmg)
+	var imageWeFound string
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.HasSuffix(path, imageToFind) {
+				imageWeFound = path
+			}
+			return nil
+		})
+	if err != nil {
+		return "", err
+	}
+	if imageWeFound != "" {
+		return imageWeFound, nil
+	}
+	return "", fmt.Errorf("image not found")
+}
+
+func validateBaseDirAndLookForImage(baseDir string, version string) (string, error) {
 	images := path.Join(baseDir, imagepath)
 	dirHandle, err := os.Open(images)
 	defer dirHandle.Close()
 	if err != nil {
 		err := os.MkdirAll(images, 0777)
 		if err != nil {
-			return false, err
+			return "", err
 		}
-		return false, nil
+		return "", nil
 	}
 
-	dmgPath := path.Join(images, version, developerDiskImageDmg)
-	dmgHandle, err := os.Open(dmgPath)
+	dmgPath, err := findImage(baseDir, version)
 	if err != nil {
-		return false, nil
+		return "", nil
 	}
-	_, err = dmgHandle.Stat()
-	if err != nil {
-		return false, nil
-	}
-	return true, nil
+
+	return dmgPath, nil
 }
 
 // DownloadFile will download a url to a local file. It's efficient because it will
