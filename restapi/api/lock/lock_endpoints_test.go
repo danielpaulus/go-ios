535d5ede30c67d97d17ce700316d0b55bdfed69c
N.Shabanov <drunkencheetah@outlook.com>
2022-10-22 16:02:09 +0300

    extend tests

--- restapi/api/lock/lock_endpoints_test.go
+++ restapi/api/lock/lock_endpoints_test.go
@@ -13,6 +13,7 @@ import (
 )
 
 var randomDeviceUDID string
+var r *gin.Engine
 
 func setupRouter() *gin.Engine {
 	randomDeviceUDID = randomLockID()
@@ -33,71 +34,48 @@ func fakeDeviceMiddleware() gin.HandlerFunc {
 	}
 }
 
+// TESTS
 func TestDeviceLock(t *testing.T) {
-	r := setupRouter()
+	r = setupRouter()
 	responseRecorder := httptest.NewRecorder()
 
-	lockRequest, err := http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, lockRequest)
+	// Lock the device
+	lockRequest := postLock(t, responseRecorder)
 	require.Equal(t, http.StatusOK, responseRecorder.Code, "POST to %v was unsuccessful", lockRequest.URL)
-
 	validateSuccessfulLock(t, responseRecorder)
 }
 
 func TestDeviceLockAlreadyLocked(t *testing.T) {
-	r := setupRouter()
+	r = setupRouter()
 	responseRecorder := httptest.NewRecorder()
 
-	lockRequest, err := http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, lockRequest)
+	// Lock the device
+	lockRequest := postLock(t, responseRecorder)
 	require.Equal(t, http.StatusOK, responseRecorder.Code, "Initial POST to %v was unsuccessful", lockRequest.URL)
-
 	validateSuccessfulLock(t, responseRecorder)
 
-	lockRequest, err = http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, lockRequest)
+	// Try to lock the already locked device
+	lockRequest = postLock(t, responseRecorder)
 	require.Equal(t, http.StatusOK, responseRecorder.Code, "Second POST to %v was unsuccessful", lockRequest.URL)
-
 	validateDeviceAlreadyLocked(t, responseRecorder)
 }
 
 func TestLockedDeviceInGetLockedDevicesList(t *testing.T) {
-	r := setupRouter()
+	r = setupRouter()
 	responseRecorder := httptest.NewRecorder()
 
-	lockRequest, err := http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, lockRequest)
+	// Lock the device
+	lockRequest := postLock(t, responseRecorder)
 	require.Equal(t, http.StatusOK, responseRecorder.Code, "POST to %v was unsuccessful", lockRequest.URL)
-
 	lockID := validateSuccessfulLock(t, responseRecorder)
 
-	getDevicesRequest, err := http.NewRequest("GET", "/locks", nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, getDevicesRequest)
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET /locks was unsuccessful")
+	// Validate device is in /locks list
+	getDevicesRequest := getDeviceLocks(t, responseRecorder)
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET %v was unsuccessful", getDevicesRequest.URL)
 
 	var devicesResponse []lockedDevice
 	responseData, _ := ioutil.ReadAll(responseRecorder.Body)
-	err = json.Unmarshal(responseData, &devicesResponse)
+	err := json.Unmarshal(responseData, &devicesResponse)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -118,27 +96,18 @@ func TestDeletingDeviceLock(t *testing.T) {
 	r := setupRouter()
 	responseRecorder := httptest.NewRecorder()
 
-	lockRequest, err := http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, lockRequest)
+	// Lock the device
+	lockRequest := postLock(t, responseRecorder)
 	require.Equal(t, http.StatusOK, responseRecorder.Code, "POST to %v was unsuccessful", lockRequest.URL)
-
 	lockID := validateSuccessfulLock(t, responseRecorder)
 
-	getDevicesRequest, err := http.NewRequest("GET", "/locks", nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, getDevicesRequest)
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET /locks was unsuccessful")
+	// Validate device is in /locks list
+	getDevicesRequest := getDeviceLocks(t, responseRecorder)
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET %v was unsuccessful", getDevicesRequest.URL)
 
 	var devicesResponse []lockedDevice
 	responseData, _ := ioutil.ReadAll(responseRecorder.Body)
-	err = json.Unmarshal(responseData, &devicesResponse)
+	err := json.Unmarshal(responseData, &devicesResponse)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -151,21 +120,16 @@ func TestDeletingDeviceLock(t *testing.T) {
 			require.Equal(t, device.UDID, randomDeviceUDID, "Device UDID does not correspond to the LockID, expected UDID=%v, got=%v", randomDeviceUDID, device.UDID)
 		}
 	}
-
 	require.True(t, lockid_exists, "Could not find `lock_id`=%v in GET /locks response", lockID)
 
-	deleteLockRequest, err := http.NewRequest("DELETE", "/lock/"+randomDeviceUDID, nil)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	r.ServeHTTP(responseRecorder, deleteLockRequest)
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "DELETE to %v was unsuccessful", deleteLockRequest.URL)
-
+	// Delete device lock
+	deleteLockRequest := deleteLock(t, responseRecorder)
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "DELETE %v was unsuccessful", deleteLockRequest.URL)
 	validateDeviceUnlocked(t, responseRecorder)
 
+	// Validate no devices present in /locks response
 	r.ServeHTTP(responseRecorder, getDevicesRequest)
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET /locks was unsuccessful")
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "GET %v was unsuccessful", getDevicesRequest.URL)
 
 	var noLockedDevicesResponse genericLockResponse
 	responseData, _ = ioutil.ReadAll(responseRecorder.Body)
@@ -179,18 +143,49 @@ func TestDeletingDeviceLock(t *testing.T) {
 }
 
 func TestValidateDeviceNotLocked(t *testing.T) {
-	r := setupRouter()
+	r = setupRouter()
 	responseRecorder := httptest.NewRecorder()
 
+	// Validate device not locked response
+	deleteLockRequest := deleteLock(t, responseRecorder)
+	require.Equal(t, http.StatusNotFound, responseRecorder.Code, "DELETE to %v was unsuccessful", deleteLockRequest.URL)
+	validateNotLocked(t, responseRecorder)
+}
+
+// HELPER FUNCTIONS
+
+func postLock(t *testing.T, responseRecorder *httptest.ResponseRecorder) *http.Request {
+	lockRequest, err := http.NewRequest("POST", "/lock/"+randomDeviceUDID, nil)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	r.ServeHTTP(responseRecorder, lockRequest)
+	require.Equal(t, http.StatusOK, responseRecorder.Code, "POST to %v was unsuccessful", lockRequest.URL)
+
+	return lockRequest
+}
+
+func deleteLock(t *testing.T, responseRecorder *httptest.ResponseRecorder) *http.Request {
 	deleteLockRequest, err := http.NewRequest("DELETE", "/lock/"+randomDeviceUDID, nil)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
 	r.ServeHTTP(responseRecorder, deleteLockRequest)
-	require.Equal(t, http.StatusNotFound, responseRecorder.Code, "DELETE to %v was unsuccessful", deleteLockRequest.URL)
 
-	validateNotLocked(t, responseRecorder)
+	return deleteLockRequest
+}
+
+func getDeviceLocks(t *testing.T, responseRecorder *httptest.ResponseRecorder) *http.Request {
+	getDevicesRequest, err := http.NewRequest("GET", "/locks", nil)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	r.ServeHTTP(responseRecorder, getDevicesRequest)
+
+	return getDevicesRequest
 }
 
 func validateSuccessfulLock(t *testing.T, responseRecorder *httptest.ResponseRecorder) string {
