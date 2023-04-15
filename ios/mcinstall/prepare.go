adc9e383574b2c3d0e12ab0a28e8f31538b1a606
Daniel Paulus <daniel.paulus@mesmerhq.com>
2023-04-15 21:59:24 +0200

    s

---
+++ ios/mcinstall/prepare.go
@@ -0,0 +1,5 @@
+package mcinstall
+
+/*
+{"RequestType":"SetCloudConfiguration","CloudConfiguration":{"SkipSetup":["Location","Restore","SIMSetup","Android","AppleID","Siri","ScreenTime","Diagnostics","SoftwareUpdate","Passcode","Biometric","Payment","Zoom","DisplayTone","MessagingActivationUsingPhoneNumber","HomeButtonSensitivity","CloudStorage","ScreenSaver","TapToSetup","Keyboard","PreferredLanguage","SpokenLanguage","WatchMigration","OnBoarding","TVProviderSignIn","TVHomeScreenSync","Privacy","TVRoom","iMessageAndFaceTime","AppStore","Safety","TermsOfAddress","Welcome","Appearance","RestoreCompleted","UpdateCompleted"],"AllowPairing":true}}
+*/
