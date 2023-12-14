6a57c05ec38e641fb96082e5411d0805ab23ab1e
David Missmann <david.missmann@saucelabs.com>
2023-12-14 11:36:15 +0100

    Listing and killing processes

--- ios/appservice/appservice.go
+++ ios/appservice/appservice.go
@@ -2,17 +2,19 @@ package appservice
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
-	"strings"
-
 	"github.com/danielpaulus/go-ios/ios"
 	"github.com/danielpaulus/go-ios/ios/xpc"
 	"github.com/google/uuid"
 	"howett.net/plist"
+	"path"
+	"syscall"
 )
 
 type Connection struct {
-	conn *xpc.Connection
+	conn     *xpc.Connection
+	deviceId string
 }
 
 func New(deviceEntry ios.DeviceEntry) (*Connection, error) {
@@ -21,15 +23,20 @@ func New(deviceEntry ios.DeviceEntry) (*Connection, error) {
 		return nil, err
 	}
 
-	return &Connection{conn: xpcConn}, nil
+	return &Connection{conn: xpcConn, deviceId: uuid.New().String()}, nil
 }
 
 type AppLaunch struct {
 	Pid int64
 }
 
-func (c *Connection) LaunchApp(deviceId string, bundleId string, args []interface{}, env map[string]interface{}, opt map[string]interface{}) (AppLaunch, error) {
-	msg := buildAppLaunchPayload(deviceId, bundleId, args, env, opt)
+type Process struct {
+	Pid  int
+	Path string
+}
+
+func (c *Connection) LaunchApp(deviceId string, bundleId string, args []interface{}, env map[string]interface{}) (AppLaunch, error) {
+	msg := buildAppLaunchPayload(c.deviceId, bundleId, args, env)
 	err := c.conn.Send(msg, xpc.HeartbeatRequestFlag)
 	m, err := c.conn.ReceiveOnServerClientStream()
 	if err != nil {
@@ -46,15 +53,73 @@ func (c *Connection) Close() error {
 	return c.conn.Close()
 }
 
-func buildAppLaunchPayload(deviceId string, bundleId string, args []interface{}, env map[string]interface{}, opt map[string]interface{}) map[string]interface{} {
+func (c *Connection) ListProcesses() ([]Process, error) {
+	req := buildListProcessesPayload(c.deviceId)
+	err := c.conn.Send(req, xpc.HeartbeatRequestFlag)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.conn.ReceiveOnServerClientStream()
+	if err != nil {
+		return nil, err
+	}
+
+	if output, ok := res["CoreDevice.output"].(map[string]interface{}); ok {
+		if tokens, ok := output["processTokens"].([]interface{}); ok {
+			processes := make([]Process, len(tokens), len(tokens))
+			for i, t := range tokens {
+				var p Process
+
+				if processMap, ok := t.(map[string]interface{}); ok {
+					if pid, ok := processMap["processIdentifier"].(int64); ok {
+						p.Pid = int(pid)
+					} else {
+						return nil, fmt.Errorf("could not parse pid (type: %T)", processMap["processIdentifier"])
+					}
+					if processPath, ok := processMap["executableURL"].(map[string]interface{})["relative"].(string); ok {
+						p.Path = processPath
+					} else {
+						return nil, fmt.Errorf("could not parse process path (type: %T)", processMap["executableURL"])
+					}
+				} else {
+					return nil, errors.New("could not get process info")
+				}
+
+				processes[i] = p
+			}
+			return processes, nil
+		}
+	}
+
+	return nil, fmt.Errorf("could not parse response")
+}
+
+func (c *Connection) KillProcess(pid int) error {
+	req := buildSendSignalPayload(c.deviceId, pid, syscall.SIGKILL)
+	err := c.conn.Send(req, xpc.HeartbeatRequestFlag)
+	if err != nil {
+		return err
+	}
+	m, err := c.conn.ReceiveOnServerClientStream()
+	if err != nil {
+		return err
+	}
+	err = getError(m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func buildAppLaunchPayload(deviceId string, bundleId string, args []interface{}, env map[string]interface{}) map[string]interface{} {
 	platformSpecificOptions := bytes.NewBuffer(nil)
 	plistEncoder := plist.NewBinaryEncoder(platformSpecificOptions)
-	err := plistEncoder.Encode(opt)
+	err := plistEncoder.Encode(map[string]interface{}{})
 	if err != nil {
 		panic(err)
 	}
 
-	return buildCoreDevicePayload(deviceId, "com.apple.coredevice.feature.launchapplication", map[string]interface{}{
+	return buildRequest(deviceId, "com.apple.coredevice.feature.launchapplication", map[string]interface{}{
 		"applicationSpecifier": map[string]interface{}{
 			"bundleIdentifier": map[string]interface{}{
 				"_0": bundleId,
@@ -66,7 +131,7 @@ func buildAppLaunchPayload(deviceId string, bundleId string, args []interface{},
 			"platformSpecificOptions":       platformSpecificOptions.Bytes(),
 			"standardIOUsesPseudoterminals": true,
 			"startStopped":                  false,
-			"terminateExisting":             true,
+			"terminateExisting":             false,
 			"user": map[string]interface{}{
 				"active": true,
 			},
@@ -76,20 +141,17 @@ func buildAppLaunchPayload(deviceId string, bundleId string, args []interface{},
 	})
 }
 
-func buildCoreDevicePayload(deviceId string, feature string, input map[string]interface{}) map[string]interface{} {
-	return map[string]interface{}{
-		"CoreDevice.CoreDeviceDDIProtocolVersion": int64(0),
-		"CoreDevice.action":                       map[string]interface{}{},
-		"CoreDevice.coreDeviceVersion": map[string]interface{}{
-			"components":              []interface{}{uint64(0x15c), uint64(0x1), uint64(0x0), uint64(0x0), uint64(0x0)},
-			"originalComponentsCount": int64(2),
-			"stringValue":             "348.1",
+func buildListProcessesPayload(deviceId string) map[string]interface{} {
+	return buildRequest(deviceId, "com.apple.coredevice.feature.listprocesses", nil)
+}
+
+func buildSendSignalPayload(deviceId string, pid int, signal syscall.Signal) map[string]interface{} {
+	return buildRequest(deviceId, "com.apple.coredevice.feature.sendsignaltoprocess", map[string]interface{}{
+		"process": map[string]interface{}{
+			"processIdentifier": int64(pid),
 		},
-		"CoreDevice.deviceIdentifier":     deviceId,
-		"CoreDevice.featureIdentifier":    feature,
-		"CoreDevice.input":                input,
-		"CoreDevice.invocationIdentifier": strings.ToUpper(uuid.New().String()),
-	}
+		"signal": int64(signal),
+	})
 }
 
 func pidFromResponse(response map[string]interface{}) (int64, error) {
@@ -103,12 +165,31 @@ func pidFromResponse(response map[string]interface{}) (int64, error) {
 	return 0, fmt.Errorf("could not get pid from response")
 }
 
-func (c *Connection) ListProcesses(deviceId string) (map[string]interface{}, error) {
-	msg := buildCoreDevicePayload(deviceId, "com.apple.coredevice.feature.listprocesses", map[string]interface{}{})
-	err := c.conn.Send(msg, xpc.HeartbeatRequestFlag)
-	if err != nil {
-		return nil, err
+func buildRequest(deviceId, feature string, input map[string]interface{}) map[string]interface{} {
+	u := uuid.New()
+	return map[string]interface{}{
+		"CoreDevice.CoreDeviceDDIProtocolVersion": int64(0),
+		"CoreDevice.action":                       map[string]interface{}{},
+		"CoreDevice.coreDeviceVersion": map[string]interface{}{
+			"components":              []interface{}{uint64(0x15c), uint64(0x1), uint64(0x0), uint64(0x0), uint64(0x0)},
+			"originalComponentsCount": int64(2),
+			"stringValue":             "348.1",
+		},
+		"CoreDevice.deviceIdentifier":     deviceId,
+		"CoreDevice.featureIdentifier":    feature,
+		"CoreDevice.input":                input,
+		"CoreDevice.invocationIdentifier": u.String(),
 	}
+}
 
-	return c.conn.ReceiveOnServerClientStream()
+func getError(response map[string]interface{}) error {
+	if e, ok := response["CoreDevice.error"].(map[string]interface{}); ok {
+		return fmt.Errorf("device returned error: %+v", e)
+	}
+	return nil
+}
+
+func (p Process) ExecutableName() string {
+	_, file := path.Split(p.Path)
+	return file
 }
