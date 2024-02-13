24c3e1b0cba8c776ac2583d35b3bb81c90215e53
David Missmann <david.missmann@saucelabs.com>
2024-02-13 20:10:30 +0100

    extract handshake attempt so that defer is called immediately

--- ios/discover.go
+++ ios/discover.go
@@ -55,21 +55,24 @@ func checkEntry(ctx context.Context, device DeviceEntry, interfaceName string, e
 				continue
 			}
 			for _, ip6 := range entry.AddrIPv6 {
-				log.WithField("adrr", ip6).WithField("ifce", interfaceName).Info("query addr")
-				addr := fmt.Sprintf("%s%%%s", ip6.String(), interfaceName)
-				s, err := NewWithAddr(addr)
-				if err != nil {
-					continue
-				}
-				defer s.Close()
-				h, err := s.Handshake()
-				if err != nil {
-					continue
-				}
-				if device.Properties.SerialNumber == h.Udid {
-					result <- addr
-				}
+				tryHandshake(ip6, interfaceName, device.Properties.SerialNumber, result)
 			}
 		}
+	}
+}
+
+func tryHandshake(ip6 net.IP, interfaceName, udid string, result chan<- string) {
+	addr := fmt.Sprintf("%s%%%s", ip6.String(), interfaceName)
+	s, err := NewWithAddr(addr)
+	if err != nil {
+		return
+	}
+	defer s.Close()
+	h, err := s.Handshake()
+	if err != nil {
+		return
+	}
+	if udid == h.Udid {
+		result <- addr
 	}
 }
