40a87999c1ee56769253959ef44da8bbfb778063
danielpaulus <daniel.paulus@icloud.com>
2024-02-14 21:00:18 +0100

    Update ncm/linux_commands.go
    Co-authored-by: dmissmann <37073203+dmissmann@users.noreply.github.com>

--- ncm/linux_commands.go
+++ ncm/linux_commands.go
@@ -10,7 +10,7 @@ import (
 
 // works on ubuntu
 func SetInterfaceUp(interfaceName string) (string, error) {
-	b, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo ip link set dev %s up", interfaceName)).CombinedOutput()
+	b, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("ip link set dev %s up", interfaceName)).CombinedOutput()
 	return string(b), err
 }
 
