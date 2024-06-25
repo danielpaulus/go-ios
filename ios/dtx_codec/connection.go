e005c125a10d9a8b84610f04c6b1c091c413c43a
David Missmann <david.missmann@saucelabs.com>
2024-06-25 13:26:15 +0200

    buffer the dtx connection reads

--- ios/dtx_codec/connection.go
+++ ios/dtx_codec/connection.go
@@ -1,6 +1,7 @@
 package dtx
 
 import (
+	"bufio"
 	"errors"
 	"io"
 	"math"
@@ -166,8 +167,8 @@ func (dtxConn *Connection) Send(message []byte) error {
 
 // reader reads messages from the byte stream and dispatches them to the right channel when they are decoded.
 func reader(dtxConn *Connection) {
+	reader := bufio.NewReaderSize(dtxConn.deviceConnection.Reader(), 512*1024)
 	for {
-		reader := dtxConn.deviceConnection.Reader()
 		msg, err := ReadMessage(reader)
 		if err != nil {
 			defer dtxConn.close(err)
