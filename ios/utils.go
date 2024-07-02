e04bd84fb5113e79d1b6cb919dbee6f3a8070e58
danielpaulus <daniel.paulus@icloud.com>
2024-07-02 22:25:26 +0200

    move unzip util to ios.Utils, fix image auto command for 17+ (#420)

--- ios/utils.go
+++ ios/utils.go
@@ -1,10 +1,13 @@
 package ios
 
 import (
+	"archive/zip"
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 
@@ -198,4 +201,63 @@ func GenericSliceToType[T any](input []interface{}) ([]T, error) {
 		}
 	}
 	return result, nil
+}
+
+// Unzip is code I copied from https://golangcode.com/unzip-files-in-go/
+// thank you guys for the cool helpful code examples :-D
+func Unzip(src string, dest string) ([]string, uint64, error) {
+	var overallSize uint64
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, 0, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, 0, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, 0, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, 0, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, 0, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+		// sizeStat, err := outFile.Stat()
+		overallSize += f.UncompressedSize64
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, 0, err
+		}
+	}
+	return filenames, overallSize, nil
 }
