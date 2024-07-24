5d31df4e88805996b54fd18fe56141b40dfbc1bb
Diego Perini <diego.perini@saucelabs.com>
2024-07-24 11:35:57 +0300

    Simplified dtx decoder tests and added buffers to all readers in tests

--- ios/dtx_codec/decoder_test.go
+++ ios/dtx_codec/decoder_test.go
@@ -162,7 +162,7 @@ func TestFragmentedMessage(t *testing.T) {
 	nonblockingFullMessage := defragmenter.Extract()
 
 	// now test that the blocking decoder creates the same message and that it is decodeable
-	dtxReader := bytes.NewReader(dat)
+	dtxReader := bufio.NewReader(bytes.NewReader(dat))
 	msg, err = dtx.ReadMessage(dtxReader)
 	if assert.NoError(t, err) {
 		assert.Equal(t, uint16(3), msg.Fragments)
@@ -188,53 +188,12 @@ func TestFragmentedMessage(t *testing.T) {
 	assert.Equal(t, true, defragmenter.HasFinished())
 	defraggedMessage := defragmenter.Extract()
 	assert.Equal(t, defraggedMessage, nonblockingFullMessage)
-	dtxReader = bytes.NewReader(defraggedMessage)
+	dtxReader = bufio.NewReader(bytes.NewReader(defraggedMessage))
 	_, err = dtx.ReadMessage(dtxReader)
 	assert.NoError(t, err)
 }
 
 func TestDecoder(t *testing.T) {
-	dat, err := os.ReadFile("fixtures/notifyOfPublishedCapabilites")
-	if err != nil {
-		t.Fatal(err)
-	}
-	msg, remainingBytes, err := dtx.DecodeNonBlocking(dat)
-	if assert.NoError(t, err) {
-		assert.Equal(t, 0, len(remainingBytes))
-		assert.Equal(t, msg.Fragments, uint16(1))
-		assert.Equal(t, msg.FragmentIndex, uint16(0))
-		assert.Equal(t, msg.MessageLength, 612)
-		assert.Equal(t, 0, msg.ChannelCode)
-		assert.Equal(t, false, msg.ExpectsReply)
-		assert.Equal(t, 2, msg.Identifier)
-		assert.Equal(t, 0, msg.ChannelCode)
-
-		assert.Equal(t, dtx.MessageType(2), msg.PayloadHeader.MessageType)
-		assert.Equal(t, uint32(425), msg.PayloadHeader.AuxiliaryLength)
-		assert.Equal(t, uint32(596), msg.PayloadHeader.TotalPayloadLength)
-		assert.Equal(t, uint32(0), msg.PayloadHeader.Flags)
-
-	}
-
-	msg, err = dtx.ReadMessage(bytes.NewReader(dat))
-	if assert.NoError(t, err) {
-
-		assert.Equal(t, msg.Fragments, uint16(1))
-		assert.Equal(t, msg.FragmentIndex, uint16(0))
-		assert.Equal(t, msg.MessageLength, 612)
-		assert.Equal(t, 0, msg.ChannelCode)
-		assert.Equal(t, false, msg.ExpectsReply)
-		assert.Equal(t, 2, msg.Identifier)
-		assert.Equal(t, 0, msg.ChannelCode)
-
-		assert.Equal(t, dtx.MessageType(2), msg.PayloadHeader.MessageType)
-		assert.Equal(t, uint32(425), msg.PayloadHeader.AuxiliaryLength)
-		assert.Equal(t, uint32(596), msg.PayloadHeader.TotalPayloadLength)
-		assert.Equal(t, uint32(0), msg.PayloadHeader.Flags)
-	}
-}
-
-func TestDecoderWithBufferedReader(t *testing.T) {
 	dat, err := os.ReadFile("fixtures/notifyOfPublishedCapabilites")
 	if err != nil {
 		t.Fatal(err)
