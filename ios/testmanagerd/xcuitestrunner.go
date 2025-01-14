0c06384c8509d8dd978c43e56fafc209c072a243
Cristian Pop <cristian.pop@saucelabs.com>
2025-01-14 15:04:17 +0100

    Add missing proxydispatcher cases + fix productModuleName for iOS <17 (#547)
    The method switch in proxydispatcher::Dispatch was missing a few cases, which led to missing XCUITest results when using go-ios on iOS <17 devices.
    
    Additionally, the productModuleName field in the XCTestConfiguration appeared to be wrong (also for iOS <17 devices), leading to the same issue of missing test results. It has been changed to match the iOS 17 config, which seems to work better for the test cases I've tried.

--- ios/testmanagerd/xcuitestrunner.go
+++ ios/testmanagerd/xcuitestrunner.go
@@ -554,7 +554,8 @@ func createTestConfigOnDevice(testSessionID uuid.UUID, info testInfo, houseArres
 
 	testBundleURL := path.Join(info.testApp.path, "PlugIns", xctestConfigFileName)
 
-	config := nskeyedarchiver.NewXCTestConfiguration(info.targetApp.bundleName, testSessionID, info.targetApp.bundleID, info.targetApp.path, testBundleURL, testsToRun, testsToSkip, isXCTest, version)
+	productModuleName := strings.ReplaceAll(xctestConfigFileName, ".xctest", "")
+	config := nskeyedarchiver.NewXCTestConfiguration(productModuleName, testSessionID, info.targetApp.bundleID, info.targetApp.path, testBundleURL, testsToRun, testsToSkip, isXCTest, version)
 	result, err := nskeyedarchiver.ArchiveXML(config)
 	if err != nil {
 		return "", nskeyedarchiver.XCTestConfiguration{}, err
@@ -564,7 +565,7 @@ func createTestConfigOnDevice(testSessionID uuid.UUID, info testInfo, houseArres
 	if err != nil {
 		return "", nskeyedarchiver.XCTestConfiguration{}, err
 	}
-	return xctestConfigPath, nskeyedarchiver.NewXCTestConfiguration(info.targetApp.bundleName, testSessionID, info.targetApp.bundleID, info.targetApp.path, testBundleURL, testsToRun, testsToSkip, isXCTest, version), nil
+	return xctestConfigPath, nskeyedarchiver.NewXCTestConfiguration(productModuleName, testSessionID, info.targetApp.bundleID, info.targetApp.path, testBundleURL, testsToRun, testsToSkip, isXCTest, version), nil
 }
 
 func createTestConfig(info testInfo, testSessionID uuid.UUID, xctestConfigFileName string, testsToRun []string, testsToSkip []string, isXCTest bool, version *semver.Version) nskeyedarchiver.XCTestConfiguration {
