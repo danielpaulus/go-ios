b8c34633ff794e26e3112d6f55cecd4bb1cb2b39
Victor <victor.kachalov@proton.me>
2025-01-14 08:38:48 -0600

    Reset ax settings (#546)
    Authored-by: fish-sauce <victor.kachalov@saucelabs.com>

--- ios/accessibility/utils.go
+++ ios/accessibility/utils.go
@@ -1,10 +1,15 @@
 package accessibility
 
-func convertToStringList(payload []interface{}) []string {
+import "fmt"
+
+func convertToStringList(payload []interface{}) ([]string, error) {
+	if len(payload) != 1 {
+		return nil, fmt.Errorf("invalid payload length %d", len(payload))
+	}
 	list := payload[0].([]interface{})
 	result := make([]string, len(list))
 	for i, v := range list {
 		result[i] = v.(string)
 	}
-	return result
+	return result, nil
 }
