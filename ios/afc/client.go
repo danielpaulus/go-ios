ccc157af5d9fc25aebd4d5714b627bb1b4a90ed8
David Missmann <david.missmann@saucelabs.com>
2025-07-04 13:22:45 +0200

    implement new client for AFC

---
+++ ios/afc/client.go
@@ -0,0 +1,420 @@
+package afc
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"path"
+	"strconv"
+	"strings"
+	"sync/atomic"
+
+	"github.com/danielpaulus/go-ios/ios"
+	"golang.org/x/exp/slices"
+)
+
+// WalkFunc is used by [Client.WalkDir] for traversing directories
+// This function will be called for each entry in a directory
+// Execution can be controlled by returning error values from this function:
+//   - [fs.SkipDir] will skip files of the current directory
+//   - [fs.SkipAll] will stop traversal and exit without an error
+//   - returning any other non-nil error will stop traversal and return this error from [Client.WalkDir]
+type WalkFunc func(path string, info FileInfo, err error) error
+
+type Client struct {
+	connection io.ReadWriteCloser
+	packetNum  atomic.Int64
+}
+
+func NewAfcConnection(d ios.DeviceEntry) (*Client, error) {
+	deviceConn, err := ios.ConnectToService(d, serviceName)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to service '%s': %w", serviceName, err)
+	}
+	return &Client{
+		connection: deviceConn,
+	}, nil
+}
+
+func (c *Client) Close() error {
+	err := c.connection.Close()
+	if err != nil {
+		return fmt.Errorf("error closing afc client: %w", err)
+	}
+	return nil
+}
+
+// List all entries of the provided path
+func (c *Client) List(p string) ([]string, error) {
+	err := c.sendPacket(Afc_operation_read_dir, []byte(p), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error listing afc dir: %w", err)
+	}
+	pack, err := c.readPacket()
+	if err != nil {
+		return nil, fmt.Errorf("error listing afc dir: %w", err)
+	}
+	reader := bufio.NewReader(bytes.NewReader(pack.Payload))
+	var list []string
+	for {
+		s, err := reader.ReadString('\x00')
+		if err != nil {
+			break
+		}
+		if len(s) == 0 {
+			continue
+		}
+		list = append(list, s[:len(s)-1])
+	}
+	return list, nil
+}
+
+func (c *Client) Open(p string, mode Mode) (*File, error) {
+	pathBytes := []byte(p)
+	pathBytes = append(pathBytes, 0)
+	headerLength := 8 + uint64(len(pathBytes))
+	headerPayload := make([]byte, headerLength)
+	binary.LittleEndian.PutUint64(headerPayload, uint64(mode))
+	copy(headerPayload[8:], pathBytes)
+	err := c.sendPacket(Afc_operation_file_open, headerPayload, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+	resp, err := c.readPacket()
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+	fd := binary.LittleEndian.Uint64(resp.HeaderPayload)
+
+	return &File{
+		client: c,
+		handle: fd,
+	}, nil
+}
+
+// CreateDir
+func (c *Client) CreateDir(p string) error {
+	headerPayload := []byte(p)
+	headerPayload = append(headerPayload, 0)
+
+	err := c.sendPacket(Afc_operation_make_dir, headerPayload, nil)
+	if err != nil {
+		return fmt.Errorf("error creating dir: %w", err)
+	}
+	_, err = c.readPacket()
+	if err != nil {
+		return fmt.Errorf("error creating dir: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) Delete(p string) error {
+	headerPayload := []byte(p)
+	err := c.sendPacket(Afc_operation_remove_path_and_contents, headerPayload, nil)
+	if err != nil {
+		return fmt.Errorf("error deleting file: %w", err)
+	}
+	_, err = c.readPacket()
+	if err != nil {
+		return fmt.Errorf("error deleting file: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) sendPacket(operation uint64, headerPayload []byte, payload []byte) error {
+	num := c.packetNum.Add(1)
+
+	thisLen := Afc_header_size + uint64(len(headerPayload))
+	p := packet{
+		Header: header{
+			Magic:     Afc_magic,
+			EntireLen: thisLen + uint64(len(payload)),
+			ThisLen:   thisLen,
+			PacketNum: uint64(num),
+			Operation: operation,
+		},
+		HeaderPayload: headerPayload,
+		Payload:       payload,
+	}
+
+	err := binary.Write(c.connection, binary.LittleEndian, p.Header)
+	if err != nil {
+		return fmt.Errorf("error writing header: %w", err)
+	}
+	if len(headerPayload) > 0 {
+		_, err = c.connection.Write(headerPayload)
+		if err != nil {
+			return fmt.Errorf("error writing header payload: %w", err)
+		}
+	}
+	if len(payload) > 0 {
+		_, err = c.connection.Write(payload)
+		if err != nil {
+			return fmt.Errorf("error writing payload: %w", err)
+		}
+	}
+	return nil
+}
+
+func (c *Client) readPacket() (packet, error) {
+	var h header
+	err := binary.Read(c.connection, binary.LittleEndian, &h)
+	if err != nil {
+		return packet{}, fmt.Errorf("error reading header: %w", err)
+	}
+	headerPayloadLen := h.ThisLen - Afc_header_size
+	payloadLen := h.EntireLen - h.ThisLen
+
+	headerpayload := make([]byte, headerPayloadLen)
+	payload := make([]byte, payloadLen)
+
+	p := packet{
+		Header:        h,
+		HeaderPayload: headerpayload,
+		Payload:       payload,
+	}
+
+	if headerPayloadLen > 0 {
+		_, err = io.ReadFull(c.connection, headerpayload)
+		if err != nil {
+			return packet{}, fmt.Errorf("error reading header: %w", err)
+		}
+	}
+	if payloadLen > 0 {
+		_, err = io.ReadFull(c.connection, payload)
+		if err != nil {
+			return packet{}, fmt.Errorf("error reading payload: %w", err)
+		}
+	}
+
+	if p.Header.Operation == Afc_operation_status {
+		code := binary.LittleEndian.Uint64(p.HeaderPayload)
+		if code == Afc_Err_Success {
+			return p, nil
+		}
+		return p, fmt.Errorf("error processing afc status: %d", code)
+	}
+
+	return p, nil
+}
+
+type FileType string
+
+const (
+	// S_IFDIR marks a directory
+	S_IFDIR FileType = "S_IFDIR"
+	// S_IFDIR marks a regular file
+	S_IFMT FileType = "S_IFMT"
+)
+
+type FileInfo struct {
+	Name string
+	Type FileType
+	Mode uint32
+	Size int64
+}
+
+func (c *Client) Stat(s string) (FileInfo, error) {
+	err := c.sendPacket(Afc_operation_file_info, []byte(s), nil)
+	if err != nil {
+		return FileInfo{}, fmt.Errorf("error getting file info: %w", err)
+	}
+
+	resp, err := c.readPacket()
+	if err != nil {
+		return FileInfo{}, fmt.Errorf("error getting file info: %w", err)
+	}
+
+	reader := bufio.NewReader(bytes.NewReader(resp.Payload))
+	info := FileInfo{}
+
+	// Parse the file info response which is a series of null-terminated strings
+	// in key-value pairs
+	for {
+		key, err := reader.ReadString('\x00')
+		if err != nil {
+			break
+		}
+		if len(key) <= 1 {
+			break
+		}
+		key = key[:len(key)-1] // Remove null terminator
+
+		value, err := reader.ReadString('\x00')
+		if err != nil {
+			break
+		}
+		value = value[:len(value)-1] // Remove null terminator
+
+		switch key {
+		case "st_ifmt":
+			info.Type = FileType(value)
+		case "st_size":
+			size, _ := strconv.ParseInt(value, 10, 64)
+			info.Size = size
+		case "st_mode":
+			mode, _ := strconv.ParseUint(value, 8, 32)
+			info.Mode = uint32(mode)
+		}
+	}
+
+	// Set the name from the path
+	parts := strings.Split(s, "/")
+	if len(parts) > 0 {
+		info.Name = parts[len(parts)-1]
+	}
+
+	return info, nil
+}
+
+func (c *Client) WalkDir(p string, f WalkFunc) error {
+	files, err := c.List(p)
+	if err != nil {
+		return err
+	}
+
+	slices.Sort(files)
+	for _, file := range files {
+		if file == "." || file == ".." {
+			continue
+		}
+		info, err := c.Stat(path.Join(p, file))
+		if err != nil {
+			return err
+		}
+		fnErr := f(path.Join(p, file), info, nil)
+		if fnErr != nil {
+			if errors.Is(fnErr, fs.SkipDir) {
+				continue
+			} else if errors.Is(fnErr, fs.SkipAll) {
+				return nil
+			} else {
+				return fnErr
+			}
+		}
+		if info.Type == S_IFDIR {
+			walkErr := c.WalkDir(path.Join(p, file), f)
+			if walkErr != nil {
+				return walkErr
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Client) DeviceInfo() (AFCDeviceInfo, error) {
+	err := c.sendPacket(Afc_operation_device_info, nil, nil)
+	if err != nil {
+		return AFCDeviceInfo{}, fmt.Errorf("error getting device info: %w", err)
+	}
+	resp, err := c.readPacket()
+	if err != nil {
+		return AFCDeviceInfo{}, fmt.Errorf("error getting device info: %w", err)
+	}
+
+	bs := bytes.Split(resp.Payload, []byte{0})
+
+	var info AFCDeviceInfo
+	for i := 0; i+1 < len(bs); i += 2 {
+		key := string(bs[i])
+		if key == "Model" {
+			info.Model = string(bs[i+1])
+			continue
+		}
+		value, intParseErr := strconv.ParseUint(string(bs[i+1]), 10, 64)
+		switch key {
+		case "FSTotalBytes":
+			if intParseErr != nil {
+				return AFCDeviceInfo{}, fmt.Errorf("error parsing %s: %w", key, intParseErr)
+			}
+			info.TotalBytes = value
+		case "FSFreeBytes":
+			if intParseErr != nil {
+				return AFCDeviceInfo{}, fmt.Errorf("error parsing %s: %w", key, intParseErr)
+			}
+			info.FreeBytes = value
+		case "FSBlockSize":
+			if intParseErr != nil {
+				return AFCDeviceInfo{}, fmt.Errorf("error parsing %s: %w", key, intParseErr)
+			}
+			info.BlockSize = value
+		}
+	}
+	return info, nil
+}
+
+type header struct {
+	Magic     uint64
+	EntireLen uint64
+	ThisLen   uint64
+	PacketNum uint64
+	Operation uint64
+}
+
+type packet struct {
+	Header        header
+	HeaderPayload []byte
+	Payload       []byte
+}
+
+type File struct {
+	client *Client
+	handle uint64
+}
+
+func (f *File) Read(p []byte) (int, error) {
+	headerPayload := make([]byte, 16)
+	binary.LittleEndian.PutUint64(headerPayload, f.handle)
+	binary.LittleEndian.PutUint64(headerPayload[8:], uint64(len(p)))
+
+	err := f.client.sendPacket(Afc_operation_file_read, headerPayload, nil)
+	if err != nil {
+		return 0, fmt.Errorf("error reading data: %w", err)
+	}
+	resp, err := f.client.readPacket()
+	copy(p, resp.Payload)
+	return len(resp.Payload), nil
+}
+
+func (f *File) Write(p []byte) (int, error) {
+	headerPayload := make([]byte, 8)
+	binary.LittleEndian.PutUint64(headerPayload, f.handle)
+	err := f.client.sendPacket(Afc_operation_file_write, headerPayload, p)
+	if err != nil {
+		return 0, fmt.Errorf("error writing data: %w", err)
+	}
+	_, err = f.client.readPacket()
+	if err != nil {
+		return 0, fmt.Errorf("error reading data: %w", err)
+	}
+	return len(p), nil
+}
+
+func (f *File) Close() error {
+	headerPayload := make([]byte, 8)
+	binary.LittleEndian.PutUint64(headerPayload, f.handle)
+	err := f.client.sendPacket(Afc_operation_file_close, headerPayload, nil)
+	if err != nil {
+		return fmt.Errorf("error closing file: %w", err)
+	}
+	_, err = f.client.readPacket()
+	if err != nil {
+		return fmt.Errorf("error closing file: %w", err)
+	}
+	return nil
+}
+
+type Mode uint64
+
+const (
+	READ_ONLY                = Mode(0x00000001)
+	READ_WRITE_CREATE        = Mode(0x00000002)
+	WRITE_ONLY_CREATE_TRUNC  = Mode(0x00000003)
+	READ_WRITE_CREATE_TRUNC  = Mode(0x00000004)
+	WRITE_ONLY_CREATE_APPEND = Mode(0x00000005)
+	READ_WRITE_CREATE_APPEND = Mode(0x00000006)
+)
---
+++ ios/afc/client_test.go
@@ -0,0 +1,221 @@
+package afc
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"path"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/danielpaulus/go-ios/ios"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAfc(t *testing.T) {
+	devices, err := ios.ListDevices()
+	if err != nil {
+		t.Skipf("failed to list devices: %s", err)
+		return
+	}
+
+	if len(devices.DeviceList) == 0 {
+		t.Skipf("no devices connected")
+		return
+	}
+
+	for _, device := range devices.DeviceList {
+
+		t.Run(fmt.Sprintf("device %s", device.Properties.SerialNumber), func(t *testing.T) {
+
+			client, err := NewAfcConnection(device)
+			assert.NoError(t, err)
+
+			defer client.Close()
+
+			t.Run("list /tmp", func(t *testing.T) {
+				_, err := client.List("/")
+				assert.NoError(t, err)
+			})
+
+			t.Run("list invalid folder returns error", func(t *testing.T) {
+				_, err := client.List("/invalid123")
+				assert.Error(t, err)
+			})
+
+			t.Run("create file", func(t *testing.T) {
+				files, err := client.List(".")
+				assert.NoError(t, err)
+				hasFile := slices.ContainsFunc(files, func(s string) bool {
+					return strings.Contains(s, "test-file")
+				})
+				if hasFile {
+					err = client.Delete("./test-file")
+					assert.NoError(t, err)
+				}
+				f, err := client.Open("./test-file", READ_WRITE_CREATE_TRUNC)
+				assert.NoError(t, err)
+
+				err = f.Close()
+				assert.NoError(t, err)
+
+				err = client.Delete("./test-file")
+				assert.NoError(t, err)
+			})
+
+			t.Run("write to file and check size", func(t *testing.T) {
+				f, err := client.Open("./test-file", READ_WRITE_CREATE_TRUNC)
+				assert.NoError(t, err)
+
+				n, err := f.Write([]byte("test"))
+				assert.NoError(t, err)
+				assert.Equal(t, 4, n)
+
+				err = f.Close()
+				assert.NoError(t, err)
+
+				info, err := client.Stat("./test-file")
+
+				assert.EqualValues(t, 4, info.Size)
+
+				err = client.Delete("./test-file")
+				assert.NoError(t, err)
+			})
+
+			t.Run("write to file and read it back", func(t *testing.T) {
+				f, err := client.Open("./test-file", READ_WRITE_CREATE_TRUNC)
+				assert.NoError(t, err)
+
+				n, err := f.Write([]byte("test"))
+				assert.NoError(t, err)
+				assert.Equal(t, 4, n)
+
+				err = f.Close()
+				assert.NoError(t, err)
+
+				f, err = client.Open("./test-file", READ_ONLY)
+				assert.NoError(t, err)
+
+				b := make([]byte, 8)
+				n, err = f.Read(b)
+				assert.NoError(t, err)
+				assert.Equal(t, []byte("test"), b[:n])
+
+				err = client.Delete("./test-file")
+				assert.NoError(t, err)
+			})
+
+			t.Run("created and delete nested directory", func(t *testing.T) {
+				err = client.CreateDir("./some/nested/directory")
+				assert.NoError(t, err)
+
+				var info FileInfo
+				info, err = client.Stat("./some")
+				assert.NoError(t, err)
+				assert.Equal(t, S_IFDIR, info.Type)
+
+				info, err = client.Stat("./some/nested")
+				assert.NoError(t, err)
+				assert.Equal(t, S_IFDIR, info.Type)
+
+				info, err = client.Stat("./some/nested/directory")
+				assert.NoError(t, err)
+				assert.Equal(t, S_IFDIR, info.Type)
+
+				err = client.Delete("./some")
+				assert.NoError(t, err)
+
+				_, err = client.Stat("./some")
+				assert.Error(t, err)
+			})
+
+			t.Run("walk dir", func(t *testing.T) {
+				basePath := path.Join("./", uuid.New().String())
+				mustCreateDir(client, basePath)
+				mustCreateDir(client, path.Join(basePath, "a-dir"))
+				mustCreateDir(client, path.Join(basePath, "a-dir", "subdir"))
+				mustCreateFile(client, path.Join(basePath, "a-dir", "file"))
+				mustCreateDir(client, path.Join(basePath, "c-dir"))
+
+				t.Run("visit all", func(t *testing.T) {
+					var visited []string
+					err = client.WalkDir(basePath, func(path string, info FileInfo, err error) error {
+						visited = append(visited, path)
+						return nil
+					})
+
+					assert.NoError(t, err)
+					assert.Equal(t, []string{
+						path.Join(basePath, "a-dir"),
+						path.Join(basePath, "a-dir/file"),
+						path.Join(basePath, "a-dir/subdir"),
+						path.Join(basePath, "c-dir"),
+					}, visited)
+				})
+
+				t.Run("skip dir", func(t *testing.T) {
+					var visited []string
+					err = client.WalkDir(basePath, func(p string, info FileInfo, err error) error {
+						visited = append(visited, p)
+						if path.Base(p) == "a-dir" {
+							return fs.SkipDir
+						}
+						return nil
+					})
+
+					assert.NoError(t, err)
+					assert.Equal(t, []string{
+						path.Join(basePath, "a-dir"),
+						path.Join(basePath, "c-dir"),
+					}, visited)
+				})
+
+				t.Run("skip all", func(t *testing.T) {
+					var visited []string
+					err = client.WalkDir(basePath, func(p string, info FileInfo, err error) error {
+						visited = append(visited, p)
+						return fs.SkipAll
+					})
+
+					assert.NoError(t, err)
+					assert.Equal(t, []string{
+						path.Join(basePath, "a-dir"),
+					}, visited)
+				})
+
+				t.Run("return error stops walkdir", func(t *testing.T) {
+					var visited []string
+					walkDirErr := errors.New("stop walkdir")
+					err = client.WalkDir(basePath, func(p string, info FileInfo, err error) error {
+						visited = append(visited, p)
+						return walkDirErr
+					})
+					assert.Len(t, visited, 1)
+					assert.Equal(t, walkDirErr, err)
+				})
+
+				t.Run("device info", func(t *testing.T) {
+					_, err := client.DeviceInfo()
+					assert.NoError(t, err)
+				})
+			})
+		})
+	}
+}
+
+func mustCreateDir(c *Client, dir string) {
+	err := c.CreateDir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustCreateFile(c *Client, path string) {
+	f, err := c.Open(path, READ_WRITE_CREATE)
+	if err != nil {
+		panic(err)
+	}
+	_ = f.Close()
+}
