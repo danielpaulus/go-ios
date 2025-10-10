2dcf6b2136a7cf9a687f720585fdfd34f7f59910
Sakhi Mansoor <sakhi_mansoor@yahoo.com>
2025-10-10 15:41:32 +0200

    added test

--- ios/accessibility/accessibility_integration_test.go
+++ ios/accessibility/accessibility_integration_test.go
@@ -20,6 +20,7 @@ func TestIT(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer conn.TurnOff()
 
 	conn.SwitchToDevice()
 	if err != nil {
@@ -31,4 +32,43 @@ func TestIT(t *testing.T) {
 	conn.TurnOff()
 
 	// conn.EnableSelectionMode()
+}
+
+func TestMove(t *testing.T) {
+	device, err := ios.GetDevice("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn, err := accessibility.New(device)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.TurnOff()
+
+	conn.SwitchToDevice()
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn.EnableSelectionMode()
+
+	t.Run("Test Move directions", func(t *testing.T) {
+		directions := []accessibility.MoveDirection{
+			// newer ios(18+) devices sometimes doesn't bring focus to first element, so we need to move twice
+			accessibility.DirectionNext,
+			accessibility.DirectionNext,
+			accessibility.DirectionPrevious,
+		}
+
+		for _, direction := range directions {
+			t.Logf("Testing direction: %v", direction)
+			element, err := conn.Move(direction)
+			if err != nil {
+				t.Logf("Move %v failed (expected on some devices): %v", direction, err)
+				continue
+			}
+
+			t.Logf("Move %v succeeded: %+v", direction, element)
+		}
+	})
 }
